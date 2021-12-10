e6b7fce02aaf304bedcd6b68d5fb79f255cff219
Dobberzoon <danieldobsonmail@gmail.com>
2021-12-10 11:25:18 +0100

    .gitignore fixed

---
+++ cmake-build-release/CMakeFiles/runoff.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/runoff.dir/main.cpp.o"
+  "CMakeFiles/runoff.dir/main.cpp.o.d"
+  "runoff"
+  "runoff.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/runoff.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
