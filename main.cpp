a82f69562e0b5a844d8a7f6f81d8142619ebaea3
Dobberzoon <danieldobsonmail@gmail.com>
2021-12-18 20:30:32 +0100

    version 2 of program is working. This version includes: 1. flow direction 1a) added flat area console input function 1b) write to geotiff 2. flow accumulaton. TODO: clean code, add explanation comments

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <queue>
+#include <string>
 
 #include "gdal_priv.h"
 #include "cpl_conv.h"
@@ -26,12 +27,18 @@ struct Raster {
   void add_scanline(const int *line) {
     for (int i = 0; i < max_x; ++i) pixels.push_back(line[i]);
   }
-  
+
   // Fill entire raster with zeros
   void fill() {
     unsigned int total_pixels = max_x*max_y;
     for (int i = 0; i < total_pixels; ++i) pixels.push_back(0);
   }
+
+  // Fill entire raster with zeros
+  void fillOnes() {
+    unsigned int total_pixels = max_x*max_y;
+    for (int i = 0; i < total_pixels; ++i) pixels.push_back(1);
+    }
   
   // Access the value of a raster cell to read or write it
   int &operator()(int x, int y) {
@@ -82,7 +89,7 @@ int main(int argc, const char * argv[]) {
   // Open dataset
   GDALDataset  *input_dataset;
   GDALAllRegister();
-  input_dataset = (GDALDataset *)GDALOpen("/Users/danieldobson/Library/CloudStorage/OneDrive-Personal/GEOMATICS/GEO1015-Y2/geo1015.2021/hw/02/data/N56E105.hgt", GA_ReadOnly); // a nice tile I used for testing
+  input_dataset = (GDALDataset *)GDALOpen("/Users/danieldobson/Library/CloudStorage/OneDrive-Personal/GEOMATICS/GEO1015-Y2/geo1015.2021/hw/02/data/N46E008.hgt", GA_ReadOnly); // a nice tile I used for testing
   if (input_dataset == NULL) {
     std::cerr << "Couldn't open file" << std::endl;
     return 1;
@@ -127,32 +134,18 @@ int main(int argc, const char * argv[]) {
   } std::cout << "Created raster: " << input_raster.max_x << "x" << input_raster.pixels.size()/input_raster.max_y << " = " << input_raster.pixels.size() << std::endl;
 
 
-
   // Flow direction
   Raster flow_direction(input_raster.max_x, input_raster.max_y);
   flow_direction.fill();
   std::priority_queue<RasterCell, std::deque<RasterCell>> cells_to_process_flow;
+  std::vector<RasterCell> cells_to_process_accumulation;
+
   // to do
   // Create the output dataset for writing
 
-
-
-  // With this for loop I access the flow_direction raster to write the z values at (x, y)
-  for (int i=0; i<flow_direction.max_x; i++) {
-      for (int j=0; j<flow_direction.max_y; j++) {
-          int elevation = i*10;
-          int insertion_order = j * -1;
-          int x = i;
-          int z = j;
-          //RasterCell rc(x, y, elevation, insertion_order);
-          flow_direction.operator()(i, j) = z; //RasterCell is for the priority queue, not like this...
-      }
-  }
-
-    // Extract initial potential outlets from borders of grid
-
+    // Extraction of initial potential outlets (borders of input_raster)
     int countIO = 0;
-    for (int i=0, j=0; i<(input_raster.max_y - 1) && j<(input_raster.max_x - 1); i++, j++) {
+    for (int i=0, j=0; i<(input_raster.max_x - 1) && j<(input_raster.max_y - 1); i++, j++) {
         int xTop, yTop; // row and column of the cell
         int xBottom, yBottom; // row and column of the cell
         int xLeft, yLeft; // row and column of the cell
@@ -178,12 +171,17 @@ int main(int argc, const char * argv[]) {
         elevationLeft = input_raster(xLeft, yLeft);
         elevationRight = input_raster(xRight, yRight);
 
+        //std::cout << elevationTop << std::endl;
+        //std::cout << elevationBottom << std::endl;
+        //std::cout << elevationLeft << std::endl;
+        //std::cout << elevationRight << std::endl;
+
         insertion_orderTop = countIO;
-        countIO++;
+        //countIO++;
         insertion_orderBottom = countIO;
-        countIO++;
+        //countIO++;
         insertion_orderLeft = countIO;
-        countIO++;
+        //countIO++;
         insertion_orderRight = countIO;
         countIO++;
 
@@ -200,59 +198,198 @@ int main(int argc, const char * argv[]) {
 
 
     }
-    std::cout << "the count of the border extraction loop, versus the size of the priority queue: " << countIO <<
+    std::cout << "the count of the border extraction loop, versus the size of the priority queue accumulation: " << countIO <<
     ", " << cells_to_process_flow.size() << std::endl;
 
-    std::ofstream borderlog;
-    borderlog.open ("borderlog.txt");
+    /*
+    std::ofstream flow, accumulation;
+    flow.open("pq_flow.txt");
+    accumulation.open("pq_accumulation.txt");
+
+    for (int i = cells_to_process_accumulation.size() - 1; i >= 0; i--) {
+        accumulation << cells_to_process_accumulation[i] << std::endl;
+    }
 
     while (!cells_to_process_flow.empty()) {
-        //borderlog << cells_to_process_flow.top() << " \n";
+        flow << cells_to_process_flow.top() <<std::endl;
+        cells_to_process_flow.pop();
+    }
+
+    flow.close();
+    accumulation.close();
+    */
+
+    std::cout << "Do you want to indicate flat elevation values? y/n ";
+    std::string response;
+    std::getline(std::cin, response);
+
+    if (response == "y") {
+        std::cout << "Type the value of elevation of flat area (sea, lake, ocean etc.) rounded down as integer: ";
+        int flat;
+        std::cin >> flat;
+        while (flat != 0){
+            // Extraction of flat area's to add to priority queue
+            for (int i = 1; i < input_raster.max_x; i++) {
+                for (int j = 1; j < input_raster.max_y; j++) {
+
+                    if (input_raster(i, j) == flat) {
+                        RasterCell flat(i, j, input_raster(i, j), countIO);
+                        cells_to_process_flow.push(flat);
+                        countIO++;
+                    }
+
+                }
+            }
+            std::cout << "Type in another value for flat area, or 0 to cancel: ";
+            std::cin >> flat;
+        }
+    }
+    /*
+    // Extraction of flat area's to add to priority queue
+    for (int i = 1; i < input_raster.max_x; i++) {
+        for (int j = 1; j < input_raster.max_y; j++) {
+
+            //ele_log << input_raster(i, j) << std::endl;
+            int input;
+
+            if (input_raster(i, j) == 191) {
+                RasterCell flat(i, j, input_raster(i, j), countIO);
+                cells_to_process_flow.push(flat);
+                countIO++;
+            }
+
+        }
+    }
+    */
+    // Direction value mapping
+    const int north = 10, northEast = 20, east = 30, southEast = 40, south = 50, southWest = 60, west = 70, northWest = 80;
+
+    // Stack for accumulation
+    std::stack<RasterCell, std::deque<RasterCell>> stack;
+
+    // Search operation
+    while (!cells_to_process_flow.empty()) {
 
         int x, y, elevation, insertion_order;
         x = cells_to_process_flow.top().x;
         y = cells_to_process_flow.top().y;
         elevation = cells_to_process_flow.top().elevation;
-        insertion_order = cells_to_process_flow.top().insertion_order;
+        insertion_order = countIO;
 
-        
+        RasterCell r(x, y, elevation, insertion_order);
+        stack.push(r);
+
+        if (((y - 1) >= 0) && ((x - 1) >= 0) && (flow_direction(x - 1, y - 1) == 0)) {
+            flow_direction.operator()(x - 1, y - 1) = southEast;
+
+            elevation = input_raster(x - 1, y - 1);
+            RasterCell r1(x - 1, y - 1, elevation, insertion_order);
+            cells_to_process_flow.push(r1);
+            countIO++;
+        }
+
+        if (((y - 1) >= 0) && (flow_direction(x, y - 1) == 0)) {
+            flow_direction.operator()(x, y - 1) = east;
+
+            elevation = input_raster(x, y - 1);
+            RasterCell r2(x, y - 1, elevation, insertion_order);
+            cells_to_process_flow.push(r2);
+            countIO++;
+        }
+
+        if (((y - 1) >= 0) && ((x + 1) <= flow_direction.max_x) && (flow_direction(x + 1, y - 1) == 0)){
+            flow_direction.operator()(x + 1, y - 1) = northEast;
+
+            elevation = input_raster(x + 1, y - 1);
+            RasterCell r3(x + 1, y - 1, elevation, insertion_order);
+            cells_to_process_flow.push(r3);
+            countIO++;
+        }
+
+        if (((x + 1) <= flow_direction.max_x) && (flow_direction(x + 1, y) == 0)){
+            flow_direction.operator()(x + 1, y) = north;
+
+            elevation = input_raster(x + 1, y);
+            RasterCell r4(x + 1, y, elevation, insertion_order);
+            cells_to_process_flow.push(r4);
+            countIO++;
+        }
+
+        if (((x + 1) <= flow_direction.max_x) && ((y + 1) < flow_direction.max_y) && (flow_direction(x + 1, y + 1) == 0)) {
+            flow_direction.operator()(x + 1, y + 1) = northWest;
+
+            elevation = input_raster(x + 1, y + 1);
+            RasterCell r5(x + 1, y + 1, elevation, insertion_order);
+            cells_to_process_flow.push(r5);
+            countIO++;
+        }
+
+        if (((y + 1) < flow_direction.max_y) && (flow_direction(x, y + 1) == 0)) {
+            flow_direction.operator()(x, y + 1) = west;
+
+            elevation = input_raster(x, y + 1);
+            RasterCell r6(x, y + 1, elevation, insertion_order);
+            cells_to_process_flow.push(r6);
+            countIO++;
+        }
+        if (((y + 1) < flow_direction.max_y) && ((x - 1) >= 0) && (flow_direction(x - 1, y + 1) == 0)) {
+            flow_direction.operator()(x - 1, y + 1) = southWest;
+
+            elevation = input_raster(x - 1, y + 1);
+            RasterCell r7(x - 1, y + 1, elevation, insertion_order);
+            cells_to_process_flow.push(r7);
+            countIO++;
+        }
+        if (((x - 1) >= 0) && (flow_direction(x - 1, y) == 0)) {
+            flow_direction.operator()(x - 1, y) = south;
+
+            elevation = input_raster(x - 1, y);
+            RasterCell r8(x - 1, y, elevation, insertion_order);
+            cells_to_process_flow.push(r8);
+            countIO++;
+        }
+
         cells_to_process_flow.pop();
+
     }
-    borderlog.close();
-    /* // This will print whether included driver from input_data supports Create() or CreateCopy()
-     * // Spoiler alert: it's CreateCopy()
-    const char *pszFormat = "SRTMHGT";
-    GDALDriver *poDriver;
-    char **papszMetadata;
-    poDriver = GetGDALDriverManager()->GetDriverByName(pszFormat);
-    if( poDriver == NULL )
-        exit( 1 );
-    papszMetadata = poDriver->GetMetadata();
-    if( CSLFetchBoolean( papszMetadata, GDAL_DCAP_CREATE, FALSE ) )
-        printf( "Driver %s supports Create() method.\n", pszFormat );
-    if( CSLFetchBoolean( papszMetadata, GDAL_DCAP_CREATECOPY, FALSE ) )
-        printf( "Driver %s supports CreateCopy() method.\n", pszFormat );
+
+    std::cout << "this is how big stack is: " << stack.size() << std::endl;
+    /*
+    std::ofstream accumulation;
+    accumulation.open("pq_accumulation.txt");
+
+
+
+    while (!stack.empty()) {
+        accumulation << stack.top() <<std::endl;
+        stack.pop();
+    }
+
+    accumulation.close();
     */
 
-  // This loop reads all input_raster values
-  /*
-    for (int i=0; i<input_raster.max_x; i++) {
-        for (int j=0; j<input_raster.max_y; j++) {
-            //std::cout << j << std::endl;
-            std::cout << "this is the value at (" << i << ", " << j << ")" << input_raster.operator()(i, j) << std::endl;
+    GDALDataset  *output_flow_direction;
+    GDALDriver *driverGeotiff;
+    const char *const flow_dir_filename = "output_flow_direction_katrin_flat.tif";
+
+    driverGeotiff = GetGDALDriverManager()->GetDriverByName("GTiff");
+    output_flow_direction = driverGeotiff->Create(flow_dir_filename,nXSize,nYSize,1,GDT_Int32,NULL);
+    output_flow_direction->SetGeoTransform(geo_transform);
+    output_flow_direction->SetProjection(input_dataset->GetProjectionRef());
+
+    // Write flow direction to file
+    int *scanline2 = (int *)CPLMalloc(sizeof(float)*nYSize);
+    for (int i=0; i<nXSize; i++) {
+        for (int j = 0; j < nYSize; j++) {
+            scanline2[j] = (int)flow_direction(j, i);
         }
+        output_flow_direction->GetRasterBand(1)->RasterIO(GF_Write, 0, i, nYSize, 1, scanline2, nYSize, 1, GDT_Int32, 0,
+                                                          0);
     }
-  */
-    // This loop I broke while experimenting, but with added for j blabla you can read each value in the input_raster
-    /*
-    std::vector<int>
-    for (int i=0; i<input_raster.max_x; i++) {
-            std::cout << "this is the value at (" << i << ", " << j << ")" << input_raster.operator()(i, j) << std::endl;
-        }
-    }
-     */
+    CPLFree(scanline2);
+
    // Here I successfully accessed a value at (x, y) to read the z value from the created flow_direction raster
-    std::cout << "this is the value from flow_direction raster at (2, 3000): " << flow_direction(2,3000) << std::endl;
+    std::cout << "this is the value from flow_direction raster at (0, 0): " << flow_direction(0,0) << std::endl;
 
 
   // Here I will attempt to access the individual columns or rows with indexing
@@ -276,19 +413,143 @@ int main(int argc, const char * argv[]) {
     - You should have no loops in the flow directions raster. If you do, you’re doing something wrong. --> I think to fill the raster, as in line 123/124
     - If you’re having performance problems, you may crop your input DEM. However, there will be a penalty if your code can only work with tiny datasets.
     - The SRTM data is lat-long, but that doesn’t matter for LCP.*/
-  
-  // Write flow direction
-  // to do
-  
-  // Flow accumulation
+
   Raster flow_accumulation(input_raster.max_x, input_raster.max_y);
-  // to do
+  flow_accumulation.fill();
   
   // Write flow accumulation
   // to do
+
+    std::cout << "flow accumulation stack before: " << stack.size() << std::endl;
+
+    // Calculate flow accumulation
+    while (!stack.empty()) {
+        int x, y, accumulation, direction;
+        x = stack.top().x;
+        y = stack.top().y;
+        accumulation = flow_accumulation(x, y);
+        direction = flow_direction(x, y);
+        //std::cout << accumulation << std::endl;
+        //std::cout << direction << std::endl;
+
+
+        // linksboven
+        if (((y - 1) >= 0) && ((x - 1) >= 0) && (direction == northWest)) {
+            flow_accumulation.operator()(x - 1, y - 1)++;
+            //std::cout << "northwest" << std::endl;
+        }
+        // links
+        if (((y - 1) >= 0) && (direction == west)) {
+            flow_accumulation.operator()(x, y - 1)++;
+            //std::cout << "west" << std::endl;
+        }
+        // linksonder
+        if (((y - 1) >= 0) && ((x + 1) <= flow_accumulation.max_x) && (direction == southWest)) {
+            flow_accumulation.operator()(x + 1, y - 1)++;
+            //std::cout << "soutwest" << std::endl;
+        }
+        // onder
+        if  (((x + 1) <= flow_accumulation.max_x) && (direction == south)) {
+            flow_accumulation.operator()(x + 1, y)++;
+            //std::cout << "south" << std::endl;
+        }
+        // rechtsonder
+        if (((x + 1) <= flow_accumulation.max_x) && ((y + 1) < flow_accumulation.max_y) &&
+            (direction == southEast)) {
+            flow_accumulation.operator()(x + 1, y + 1)++;
+            //std::cout << "southeast" << std::endl;
+        }
+        // rechts
+        if (((y + 1) < flow_accumulation.max_y) && (direction == east)) {
+            flow_accumulation.operator()(x, y + 1)++;
+            //std::cout << "east" << std::endl;
+        }
+        // rechtsboven
+        if (((y + 1) < flow_accumulation.max_y) && ((x - 1) >= 0) && (direction == northEast)) {
+            flow_accumulation.operator()(x - 1, y + 1)++;
+            //std::cout << "northeast" << std::endl;
+        }
+        // boven
+        if (((x - 1) >= 0) && (direction == north)) {
+            flow_accumulation.operator()(x - 1, y)++;
+            //std::cout << "north" << std::endl;
+        }
+
+        stack.pop();
+
+    }
+
+    /*
+    // Calculate flow accumulation
+    while (!stack.empty()) {
+        int x, y, accumulation, direction;
+        x = stack.top().x;
+        y = stack.top().y;
+        accumulation = flow_accumulation(x, y);
+        direction = flow_direction(x, y);
+        stack.pop();
+
+        // linksboven
+        if (((y - 1) >= 0) && ((x - 1) >= 0) && (direction = northWest)) {
+             flow_accumulation.operator()(x - 1, y - 1) += accumulation;
+        }
+        // links
+        if (((y - 1) >= 0) && (direction == west)) {
+             flow_accumulation.operator()(x, y - 1) += accumulation;
+        }
+        // linksonder
+        if (((y - 1) >= 0) && ((x + 1) <= flow_accumulation.max_x) && (direction == southWest)) {
+             flow_accumulation.operator()(x + 1, y - 1) += accumulation;
+        }
+        // onder
+        if (((x + 1) <= flow_accumulation.max_x) && (direction == south)) {
+             flow_accumulation.operator()(x + 1, y) += accumulation;
+        }
+        // rechtsonder
+        if (((x + 1) <= flow_accumulation.max_x) && ((y + 1) < flow_accumulation.max_y) &&
+            (direction == southEast)) {
+             flow_accumulation.operator()(x + 1, y + 1) += accumulation;
+        }
+        // rechts
+        if (((y + 1) < flow_accumulation.max_y) && (direction == east)) {
+             flow_accumulation.operator()(x, y + 1) += accumulation;
+        }
+        // rechtsboven
+        if (((y + 1) < flow_accumulation.max_y) && ((x - 1) >= 0) && (direction == northEast)) {
+             flow_accumulation.operator()(x - 1, y + 1) += accumulation;
+        }
+        // boven
+        if (((x - 1) >= 0) && (direction == north)) {
+             flow_accumulation.operator()(x - 1, y) += accumulation;
+        }
+
+    }
+    */
+    std::cout << "flow accumulation stack after: " << stack.size() << std::endl;
+
+    GDALDataset  *output_flow_accumulation;
+    const char *const flow_acc_filename = "output_flow_accumulation_katrin_flat.tif";
+
+    output_flow_accumulation = driverGeotiff->Create(flow_acc_filename,nXSize,nYSize,1,GDT_Int32,NULL);
+    output_flow_accumulation->SetGeoTransform(geo_transform);
+    output_flow_accumulation->SetProjection(input_dataset->GetProjectionRef());
+
+    // Write flow direction to file
+    int *scanline3 = (int *)CPLMalloc(sizeof(float)*nYSize);
+    for (int i=0; i<nXSize; i++) {
+        for (int j = 0; j < nYSize; j++) {
+            scanline3[j] = (int)flow_accumulation(j, i);
+        }
+        output_flow_accumulation->GetRasterBand(1)->RasterIO(GF_Write, 0, i, nYSize, 1, scanline3, nYSize, 1, GDT_Int32, 0,
+                                                          0);
+    }
+    CPLFree(scanline3);
   
   // Close input dataset
   GDALClose(input_dataset);
+  GDALClose(output_flow_direction);
+  GDALClose(output_flow_accumulation);
+  GDALDestroyDriverManager();
 
   clock_t stop = clock();
   double elapsed = (double) (stop - start) / CLOCKS_PER_SEC;
